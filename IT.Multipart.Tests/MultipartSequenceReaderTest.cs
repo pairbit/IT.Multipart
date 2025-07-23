38b92b724b11b433824632e4d226ad370407d4cd
Ivan Tikhonov <itikhonov@mail.com>
2025-07-23 12:45:29 +0300

    tests

--- IT.Multipart.Tests/MultipartSequenceReaderTest.cs
+++ IT.Multipart.Tests/MultipartSequenceReaderTest.cs
@@ -5,6 +5,45 @@ namespace IT.Multipart.Tests;
 internal class MultipartSequenceReaderTest
 {
     [Test]
+    public void TryReadNextSectionNoStrictTest()
+    {
+        var span = "[[------WebKitFormBoundarylng3rD4syfIK3fT9\r\n"u8 +
+"Content-Disposition: form-data; name=transform; filename=\"Transform;utf8.xsl\"\r\n"u8 +
+"Content-Type: text/xml\r\n"u8 +
+"\r\n"u8 +
+"<data>mydata</data>\r\n"u8 +
+"------WebKitFormBoundarylng3rD4syfIK3fT9\r\n"u8 +
+"Content-Disposition: form-data; name=\"name\"\r\n"u8 +
+"\r\n"u8 +
+"package name\r\n"u8 +
+"------WebKitFormBoundarylng3rD4syfIK3fT9--]]"u8;
+
+        var boundary = new MultipartBoundary("\r\n------WebKitFormBoundarylng3rD4syfIK3fT9"u8.ToArray());
+        var memory = span.ToArray().AsMemory();
+
+        for (int i = 1; i <= memory.Length; i++)
+        {
+            var sequence = memory.SplitBySegments(i);
+
+            var reader = new MultipartSequenceReader(boundary, sequence);
+
+            Assert.That(reader.TryReadNextSection(out var section, isStrict: false), Is.True);
+            Assert.That(section.Headers.SequenceEqual("Content-Disposition: form-data; name=transform; filename=\"Transform;utf8.xsl\"\r\nContent-Type: text/xml"u8), Is.True);
+            Assert.That(section.Body.SequenceEqual("<data>mydata</data>"u8), Is.True);
+
+            Assert.That(reader.TryReadNextSection(out section, isStrict: false), Is.True);
+            Assert.That(section.Headers.SequenceEqual("Content-Disposition: form-data; name=\"name\""u8), Is.True);
+            Assert.That(section.Body.SequenceEqual("package name"u8), Is.True);
+
+            Assert.That(reader.TryReadNextSection(out section, isStrict: false), Is.False);
+            Assert.That(section, Is.EqualTo(default(MultipartSequenceSection)));
+
+            Assert.That(reader.TryReadNextSection(out section, isStrict: false), Is.False);
+            Assert.That(section, Is.EqualTo(default(MultipartSequenceSection)));
+        }
+    }
+
+    [Test]
     public void TryReadNextSectionTest()
     {
         var span = "------WebKitFormBoundarylng3rD4syfIK3fT9\r\n"u8 +
