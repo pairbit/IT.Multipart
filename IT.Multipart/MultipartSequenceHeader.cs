9be36453b78e15104c261f40590ed25b0be8ef66
Ivan Tikhonov <itikhonov@mail.com>
2025-07-23 13:10:03 +0300

    add MultipartSequenceHeadersReader

---
+++ IT.Multipart/MultipartSequenceHeader.cs
@@ -0,0 +1,10 @@
+﻿using System.Buffers;
+
+namespace IT.Multipart;
+
+public readonly struct MultipartSequenceHeader
+{
+    public ReadOnlySequence<byte> Name { get; init; }
+
+    public ReadOnlySequence<byte> Value { get; init; }
+}
---
+++ IT.Multipart/MultipartSequenceHeadersReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Buffers;
+
+namespace IT.Multipart;
+
+public struct MultipartSequenceHeadersReader
+{
+    private readonly ReadOnlySequence<byte> _sequence;
+    private SequencePosition _position;
+
+    public readonly ReadOnlySequence<byte> Sequence => _sequence;
+
+    public readonly SequencePosition Position => _position;
+
+    public MultipartSequenceHeadersReader(ReadOnlySequence<byte> sequence)
+    {
+        _sequence = sequence;
+        _position = sequence.Start;
+    }
+
+    public void Reset()
+    {
+        _position = _sequence.Start;
+    }
+
+    public bool TryReadNextHeader(out MultipartSequenceHeader header)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryReadNextHeaderValueByName(ReadOnlySpan<byte> name, out ReadOnlySequence<byte> value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryReadNextContentDisposition(out ReadOnlySequence<byte> value)
+        => TryReadNextHeaderValueByName("Content-Disposition"u8, out value);
+
+    public bool TryReadNextContentType(out ReadOnlySequence<byte> value)
+        => TryReadNextHeaderValueByName("Content-Type"u8, out value);
+}
