f38cd28370b38b513fd070d48c0fbe5b7b10530b
Ivan Tikhonov <itikhonov@mail.com>
2025-07-26 23:17:00 +0300

    tests

--- IT.Multipart.Tests/MultipartHeadersReaderTest.cs
+++ IT.Multipart.Tests/MultipartHeadersReaderTest.cs
@@ -3,6 +3,93 @@
 internal class MultipartHeadersReaderTest
 {
     [Test]
+    public void ReadNextHeaderTest()
+    {
+        var span = "Content-Disposition: form-data; name=\"transform\"; filename=\"Transform - utf8.xsl\"\r\nContent-Type: text/xml"u8;
+        var reader = new MultipartHeadersReader(span);
+
+        Assert.That(reader.ReadNextHeader(out var header, trimValue: false), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("Content-Disposition"u8), Is.True);
+        Assert.That(span[header.Value].SequenceEqual(" form-data; name=\"transform\"; filename=\"Transform - utf8.xsl\""u8), Is.True);
+
+        Assert.That(reader.ReadNextHeader(out header, trimValue: false), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("Content-Type"u8), Is.True);
+        Assert.That(span[header.Value].SequenceEqual(" text/xml"u8), Is.True);
+
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.End));
+        Assert.That(header, Is.EqualTo(default(MultipartHeader)));
+
+        reader.Reset();
+
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("Content-Disposition"u8), Is.True);
+        Assert.That(span[header.Value].SequenceEqual("form-data; name=\"transform\"; filename=\"Transform - utf8.xsl\""u8), Is.True);
+
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("Content-Type"u8), Is.True);
+        Assert.That(span[header.Value].SequenceEqual("text/xml"u8), Is.True);
+
+        reader.Reset();
+
+        Assert.That(reader.ReadNextContentDisposition(out var value), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[value].SequenceEqual("form-data; name=\"transform\"; filename=\"Transform - utf8.xsl\""u8), Is.True);
+
+        Assert.That(reader.ReadNextContentType(out value), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[value].SequenceEqual("text/xml"u8), Is.True);
+
+        span = "Content-Type:text/xml"u8;
+        reader = new MultipartHeadersReader(span);
+
+        Assert.That(reader.ReadNextHeader(out header, trimValue: false), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("Content-Type"u8), Is.True);
+        Assert.That(span[header.Value].SequenceEqual("text/xml"u8), Is.True);
+
+        reader.Reset();
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("Content-Type"u8), Is.True);
+        Assert.That(span[header.Value].SequenceEqual("text/xml"u8), Is.True);
+
+        span = " Content-Disposition : \n\r\t\v\f form-data; name=transform; filename=\"Transform - utf8.xsl\" \n\r\t\v\f \r\n Content-Type : \n\r\t\v\f text/xml \n\r\t\v\f "u8;
+        reader = new MultipartHeadersReader(span);
+
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual(" Content-Disposition "u8), Is.True);
+        Assert.That(span[header.Value].SequenceEqual("form-data; name=transform; filename=\"Transform - utf8.xsl\""u8), Is.True);
+
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual(" Content-Type "u8), Is.True);
+        Assert.That(span[header.Value].SequenceEqual("text/xml"u8), Is.True);
+
+        span = "empty:\r\nempty2:"u8;
+        reader = new MultipartHeadersReader(span);
+
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("empty"u8), Is.True);
+        Assert.That(span[header.Value].IsEmpty, Is.True);
+
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("empty2"u8), Is.True);
+        Assert.That(span[header.Value].IsEmpty, Is.True);
+
+        span = "empty: \n\r\t\v\f \r\nempty2: \n\r\t\v\f "u8;
+        reader = new MultipartHeadersReader(span);
+
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("empty"u8), Is.True);
+        Assert.That(span[header.Value].IsEmpty, Is.True);
+
+        Assert.That(reader.ReadNextHeader(out header), Is.EqualTo(MultipartReadingStatus.Done));
+        Assert.That(span[header.Name].SequenceEqual("empty2"u8), Is.True);
+        Assert.That(span[header.Value].IsEmpty, Is.True);
+
+        Assert.That(new MultipartHeadersReader("form-data"u8).ReadNextHeader(out header),
+            Is.EqualTo(MultipartReadingStatus.HeaderSeparatorNotFound));
+
+        Assert.That(new MultipartHeadersReader(": form-data"u8).ReadNextHeader(out header),
+            Is.EqualTo(MultipartReadingStatus.HeaderNameNotFound));
+    }
+
+    [Test]
     public void TryReadNextHeaderTest()
     {
         var span = "Content-Disposition: form-data; name=\"transform\"; filename=\"Transform - utf8.xsl\"\r\nContent-Type: text/xml"u8;
@@ -50,7 +137,7 @@ internal class MultipartHeadersReaderTest
         Assert.That(reader.TryReadNextHeader(out header), Is.True);
         Assert.That(span[header.Name].SequenceEqual("empty"u8), Is.True);
         Assert.That(span[header.Value].IsEmpty, Is.True);
-        
+
         Assert.That(reader.TryReadNextHeader(out header), Is.True);
         Assert.That(span[header.Name].SequenceEqual("empty2"u8), Is.True);
         Assert.That(span[header.Value].IsEmpty, Is.True);
